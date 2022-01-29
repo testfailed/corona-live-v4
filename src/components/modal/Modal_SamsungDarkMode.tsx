e75c1e10d3de7707e0102903c6c24898897b0def
DevChinchilla <dev.chinchilla@gmail.com>
2022-01-30 04:07:25 +0900

    fix: always show samsung dark mode alert

--- src/components/modal/Modal_SamsungDarkMode.tsx
+++ src/components/modal/Modal_SamsungDarkMode.tsx
@@ -6,25 +6,17 @@ import { css, styled } from "@styles/stitches.config";
 
 import { Modal } from "@components/Modal";
 
-import { useLocalStorage } from "@hooks/useLocalStorage";
-
 const SamsungDarkModeAlertModal: React.FC = () => {
   const [openModal, setOpenModal] = useState(false);
-
-  const [isSamsungFirstVisit, setIsSamsungFirstVisit] = useLocalStorage(
-    "is-samsung-first-visit",
-    true
-  );
 
   useEffect(() => {
     if (
       navigator.userAgent.match(/SamsungBrowser/i) &&
-      window.matchMedia("(prefers-color-scheme: dark)") &&
-      isSamsungFirstVisit === true
+      window.matchMedia("(prefers-color-scheme: dark)")
     ) {
       setOpenModal(true);
     }
-  }, [isSamsungFirstVisit]);
+  }, []);
 
   return (
     <Modal
@@ -41,7 +33,6 @@ const SamsungDarkModeAlertModal: React.FC = () => {
       })}
       onConfrim={(close) => {
         setOpenModal(false);
-        setIsSamsungFirstVisit(false);
         close();
       }}
       confirmText="닫기"
