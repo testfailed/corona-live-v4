88c198c593d858c815b6e1eebed6c29da9284332
DevChinchilla <dev.chinchilla@gmail.com>
2022-01-30 02:49:47 +0900

    fix: change open animation for updates modal

--- src/components/updates/Updates_Modal.tsx
+++ src/components/updates/Updates_Modal.tsx
@@ -5,13 +5,14 @@ import { rem } from "polished";
 import { css } from "@styles/stitches.config";
 
 import { Modal, ModalContent, ModalTrigger } from "@components/Modal";
+import { fadeIn } from "@styles/animations/fade-animation";
 
 const UpdatesModal: React.FC<{
   title?: string;
   triggerNode?: React.ReactNode;
 }> = ({ children, title, triggerNode }) => {
   return (
-    <Modal asChild={false} modalId="updates">
+    <Modal asChild={false} modalId="updates" transition={{ open: fadeIn }}>
       <ModalContent
         title={title ?? "실시간 확진 현황"}
         showCloseButton
