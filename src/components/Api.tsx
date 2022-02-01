77c4eef192d978f51dc667400a398b47418a311e
DevChinchilla <dev.chinchilla@gmail.com>
2022-02-01 23:17:13 +0900

    chore: create Api component

---
+++ src/components/Api.tsx
@@ -0,0 +1,17 @@
+import useApi from "@hooks/useApi";
+import { SWRConfiguration, SWRResponse } from "swr";
+
+const Api = <T,>({
+  api,
+  configs,
+  children,
+}: {
+  api: { url: string; _t: T };
+  configs?: SWRConfiguration;
+  children: (props: SWRResponse<T, any>) => JSX.Element;
+}) => {
+  const apiResponse = useApi(api, configs);
+  return children(apiResponse);
+};
+
+export default Api;
