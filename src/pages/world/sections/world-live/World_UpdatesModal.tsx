0510b30e645c19f5d299c576c184941531cab063
DevChinchilla <dev.chinchilla@gmail.com>
2022-02-12 23:55:41 +0900

    refactor: rename components

--- src/pages/world/sections/world-live/World_UpdatesModal.tsx
+++ src/pages/world/sections/world-live/World_UpdatesModal.tsx
@@ -1,31 +1,30 @@
 import React, { useMemo } from "react";
 
 import WorldApi from "@apis/world-api";
-import { transformWorldUpdates } from "@utils/world-util";
+import { transformWorldLiveUpdates } from "@utils/world-util";
 
 import Api from "@components/Api";
 import FadeIn from "@components/FadeIn";
-import UpdatesContent, {
-  UpdatesContentSkeleton,
-} from "@components/updates/Updates_Content";
-import UpdatesModal from "@components/updates/Updates_Modal";
+import LiveUpdatesContent, {
+  LiveUpdatesContentSkeleton,
+} from "@components/live-updates/LiveUpdates_Content";
+import UpdatesModal from "@components/live-updates/LiveUpdates_ModalTrigger";
 import { useTranslation } from "react-i18next";
 
 interface Props {}
 
 const WorldUpdatesModalTrigger: React.FC<Props> = ({ children }) => {
-  const { i18n } = useTranslation();
   return (
     <UpdatesModal triggerNode={children}>
       <Api api={WorldApi.updates}>
         {({ data }) => {
-          const updates = transformWorldUpdates(data?.updates ?? []);
+          const updates = transformWorldLiveUpdates(data?.updates ?? []);
           return (
             <FadeIn
               show={data?.updates !== undefined}
-              fallback={<UpdatesContentSkeleton />}
+              fallback={<LiveUpdatesContentSkeleton />}
             >
-              <UpdatesContent {...{ updates }} />
+              <LiveUpdatesContent {...{ updates }} />
             </FadeIn>
           );
         }}
